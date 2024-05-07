a98fd3af68f26ca9d83ee1ade7c0ba29f1cc7128
Arthurnacious <arthurnacious@gmail.com>
2024-05-07 14:28:22 +0200

    fade to and from slate-100

--- src/components/reviews.tsx
+++ src/components/reviews.tsx
@@ -131,6 +131,8 @@ const ReviewGrid = () => {
           />
         </>
       )}
+      <div className="pointer-events-none absolute inset-x-0 top-0 h-32 bg-gradient-to-b from-slate-100" />
+      <div className="pointer-events-none absolute inset-x-0 bottom-0 h-32 bg-gradient-to-t from-slate-100" />
     </div>
   );
 };
