572f8f180fb5017ec0452f89eae02100435b6529
Arthurnacious <arthurnacious@gmail.com>
2024-05-07 14:25:21 +0200

    fixed scroll issue(Spelling)

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -78,7 +78,7 @@ const config = {
         },
       },
       animation: {
-        marquee: "marquee var(--marquee-duration) linear infinate",
+        marquee: "marquee var(--marquee-duration) linear infinite",
         "fade-in": "fade-in 0.5s linear forwards",
         "accordion-down": "accordion-down 0.2s ease-out",
         "accordion-up": "accordion-up 0.2s ease-out",
